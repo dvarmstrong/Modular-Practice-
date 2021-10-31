4be33feb7abd985de96f42804d3f499feb5e19ff
DVA <61880632+dvarmstrong@users.noreply.github.com>
2021-10-31 19:57:01 -0400

    same commit

---
+++ parent.py
@@ -0,0 +1,22 @@
+local_var = "magical unicorns"
+def square(x):
+    return x * x
+
+class User:
+    def __init__(self, name):
+        self.name = name
+    def say_hello(self):
+        return "hello"
+
+if __name__ == "__main__":
+    print("the file is being executed directly")
+else:
+    print("The file is being executed because it is imported by another file. The file is called: ", __name__)
+
+
+
+
+print(square(5))
+user =User('Anna')
+print(user.name)
+print(__name__)
